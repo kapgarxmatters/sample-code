5986fb77d030db2dcab4fa286842fea110df7265
kapgarxmatters <kapgar@xmatters.com>
2017-11-17 08:13:13 -0700

    Create xmatters2.bat

---
+++ xmatters2.bat
@@ -0,0 +1,6 @@
+
+
+
+echo %*
+
+powershell.exe -Command Invoke-RestMethod -Body {stuff=%*} -Method POST https://[xmatters instance]/api/integration/1/functions/0c60b547-b6de-4893-bc8d-26a086237c0a/triggers?apiKey=9ce4c505-e505-4f55-b8f8-5bf51d15825c
